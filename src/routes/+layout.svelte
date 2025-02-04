264bc2f24f2115bbff36c380b445d52b9a2571ba
Tea <TeaMuc19@htl-shkoder.com>
2025-02-04 12:29:48 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let title = "Mini-Projekt";
+    import "./app.css"; 
+    let inputText = ""; 
+</script>
+
+<nav class="navbar">
+    <h1>{title}</h1>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#shop">Shop</a>
+    <a href="#contact">Contact</a>
+</nav>
+
+<div class="container">
+    <div class="box">
+        <h2>Welcome to {title}</h2>
+        <input type="text" bind:value={inputText} placeholder="Type something..." />
+        <div class="display">
+            <h1>{inputText}</h1>
+           
+        </div>
+    </div>
+</div>
+
+<footer class="footer">
+    <p>&copy; 2025 mini-project Tea Muçmeraj</p>
+</footer>
